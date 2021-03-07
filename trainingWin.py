e65f781b20983ed964efd110876443cd5f221035
Bekzhan Talgat <beka@Bekzhans-MacBook-Pro.local>
2021-03-07 09:44:26 +0600

    initial simple style

--- trainingWin.py
+++ trainingWin.py
@@ -39,6 +39,7 @@ class TrainingWin(QWidget):
 
 	def training_ui(self):
 		self.sampleTxt = QLabel(self)
+		self.sampleTxt.setObjectName("sample_text")
 		self.sampleTxt.setGeometry(285, 140, 260, 60)
 		self.sampleTxt.setFont(QFont('Times', 15))
 		self.sampleTxt.setAlignment(Qt.AlignCenter)
@@ -49,22 +50,16 @@ class TrainingWin(QWidget):
 		stx = (self.wgtW - w1)/2
 		sty = (self.wgtH - h1)/5
 		self.sampleTxt.setGeometry(stx, sty, w1, h1)
-		self.sampleTxt.setStyleSheet("QLabel"
-									"{"
-									"color : black;"
-									"border : 3px solid black;"
-									"background : white;"
-									"}")
 
 		qlw = 140
 		qlh = 25
 		w2 = int(stx+(w1-qlw)/2)
 
 		self.qle = QLineEdit(self)
-		self.qle.setGeometry(w2, sty+h1+25, qlw, qlh)
+		self.qle.setGeometry(w2, sty+h1+25, qlw, qlh+5)
 		self.qle.textChanged[str].connect(self.onChanged)
 
-		lbly = sty+h1+25
+		lbly = sty+h1+30
 		self.lbl = QLabel(self)
 		self.lbl.setText("0")
 		self.lbl.setGeometry(w2+qlw+20, lbly, 30, qlh)
@@ -73,10 +68,12 @@ class TrainingWin(QWidget):
 		pbrx = (self.wgtW-pbrW)/2
 		pbry = lbly + 120
 		self.pbar = QProgressBar(self) 
-		self.pbar.setGeometry(pbrx, pbry, pbrW, qlh)
+		self.pbar.setTextVisible(False);
+		self.pbar.setGeometry(pbrx, pbry, pbrW, qlh-10)
 
-		plby = pbry - 15
+		plby = pbry - 25
 		self.plbl = QLabel(self)
+		# self.plbl.setObjectName("progress_label")
 		self.plbl.setGeometry(pbrx+5, plby, 30, qlh)
 		self.plbl.setText("0%")
 
@@ -126,5 +123,51 @@ class TrainingWin(QWidget):
 
 if __name__ == '__main__':
 	App = QApplication(sys.argv)
+	style = """
+		QWidget{
+			background: #f2f2f2;
+		}
+
+		QLabel{
+			font-family: Arial, Helvetica, sans-serif;
+			color: #242424;
+			font-size: 15px;
+		}
+
+		QLabel#sample_text{
+			font-family: Arial, Helvetica, sans-serif;
+			padding: 5px;
+			color: #242424;
+			font-size: 18px;
+			border-style: solid;
+			border: 2px solid #242424;
+			border-radius: 10px;
+		}
+
+		QLineEdit{
+			font-family: Arial, Helvetica, sans-serif;
+			padding: 5px;
+			color: #242424;
+			font-size: 15px;
+			border: 2px solid #242424;
+			border-radius: 10px;
+		}
+
+		QProgressBar {
+			border: 2px solid #242424;
+			border-radius: 5px;
+			text-align: center;
+		}
+		QProgressBar::chunk {
+		    background-color: #242424;
+		}
+	"""
+	App.setStyleSheet(style)
+
 	window = TrainingWin()
 	App.exec()
+
+
+
+
+
